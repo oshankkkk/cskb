e5b2438fcf3c0f74e8f79a43be9e8da41f71ce27
Oshan <oshankodagoda200667@gmail.com>
2026-05-24 08:57:53 +0530

    update

--- quartz/quartz/components/Footer.tsx
+++ quartz/quartz/components/Footer.tsx
@@ -13,12 +13,11 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-		{/*{i18n(cfg.locale).components.footer.createdWith}{" "}
-         <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-		 */}
-		Created by Quartz  
-        </p>
+       <p>
+      {/* {i18n(cfg.locale).components.footer.createdWith}{" "}
+      <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year} */}
+      Created by Quartz
+    </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
